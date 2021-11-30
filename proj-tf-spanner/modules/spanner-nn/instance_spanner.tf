270d6e7074cb72e6e4ad601c0f66a1756bce9d38
arora <pythianarora@users.noreply.github.com>
2021-11-30 12:50:47 +0530

    Adding Project ID

--- proj-tf-spanner/modules/spanner-nn/instance_spanner.tf
+++ proj-tf-spanner/modules/spanner-nn/instance_spanner.tf
@@ -1,5 +1,6 @@
 resource "google_spanner_instance" "main" {
   name         = var.spanner_instance_id  # << be careful changing this in production
+  project      = var.gcp_project_id
   config       = var.spanner_config
   display_name = var.spanner_instance_id
   num_nodes    = var.spanner_num_nodes
