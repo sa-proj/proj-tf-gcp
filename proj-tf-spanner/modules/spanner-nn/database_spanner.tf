c884a0bd820cb0c271507d4508e15d62a4440b98
arora <pythianarora@users.noreply.github.com>
2021-11-30 12:57:44 +0530

    Add Project ID

--- proj-tf-spanner/modules/spanner-nn/database_spanner.tf
+++ proj-tf-spanner/modules/spanner-nn/database_spanner.tf
@@ -1,5 +1,6 @@
 resource "google_spanner_database" "database" {
   instance = google_spanner_instance.main.name
+  project      = var.gcp_project_id
   depends_on = [google_spanner_instance.main]
   name     = var.spanner_dbname
   # you can define DDL as well
