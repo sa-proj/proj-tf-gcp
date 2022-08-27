4cd4ce0b9b6b4a95b9653a609b27ab3f9493e152
Sandeep Arora <sandeep16arora@gmail.com>
2022-08-27 15:18:13 +0530

    Update variables.tf

--- proj-tf-spanner/variables.tf
+++ proj-tf-spanner/variables.tf
@@ -5,7 +5,7 @@ variable "spanner_labels" {}
 variable "spanner_dbname" {}
 variable "spanner_num_nodes" {}
 # variable "spanner_processing_units" {}
-variable "creds_file" {
+variable "creds" {
   type = string
 }
 variable "spanner_db_deletion_protection"{
